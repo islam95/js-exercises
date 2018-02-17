812e3300d700d4fd8e5f171fb8a787e9244db073
Islam Dudaev <islamwolf23@gmail.com>
2018-02-17 17:30:51 +0000

    Done exercises till L

--- week-1/K-functions-parameters/exercise4.js
+++ week-1/K-functions-parameters/exercise4.js
@@ -1,5 +1,10 @@
 // Declare your function first
+function add(a, b) {
+  var result = a + b;
+  return result;
+}
 
+var sum = add(13, 124);
 // Call the function and assign to a variable `sum`
 
 console.log(sum);
--- week-1/L-functions-nested/exercise.js
+++ week-1/L-functions-nested/exercise.js
@@ -3,3 +3,42 @@ var mentor2 = "Irina";
 var mentor3 = "Mimi";
 var mentor4 = "Rob";
 var mentor5 = "Yohannes";
+
+// -------------------------------------------------
+// Execise 2
+
+function nameUpper(name){
+    return name.toUpperCase();
+}
+
+function shout(name){
+    var message = "HELLO " + nameUpper(name);
+    return message;
+}
+
+console.log(shout(mentor1));
+console.log(shout(mentor2));
+console.log(shout(mentor3));
+console.log(shout(mentor4));
+console.log(shout(mentor5));
+
+// ---------------------------------------------------
+// Execise 1
+var numberOfStudents = 15;
+var numberOfMentors = 8;
+
+function getPercentage(number, total) {
+    var percent = Math.round((number / total) * 100);
+    return percent;
+}
+
+function message(students, mentors) {
+    var total = students + mentors;
+    var studentPercentage = getPercentage(students, total);
+    var mentorPercentage = getPercentage(mentors, total);
+
+    var message = "Percentage students: " + studentPercentage + "%\nPercentage mentors: " + mentorPercentage + "%";
+    return message;
+}
+
+console.log(message(numberOfStudents, numberOfMentors));
