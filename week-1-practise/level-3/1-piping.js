a7e6293e3f4396f4d7523ebc423b25de07d497b5
Islam Dudaev <islamwolf23@gmail.com>
2018-02-21 00:09:21 +0000

    Level-3 finished

--- week-1-practise/level-3/1-piping.js
+++ week-1-practise/level-3/1-piping.js
@@ -27,5 +27,7 @@ function format(num) {
   return "£" + num.toFixed(2);
 }
 
+var result = format(add(multiply(startingValue, 2), 10));
+
 console.log("Expected result: 20.00");
 console.log("Actual result: " + result);
